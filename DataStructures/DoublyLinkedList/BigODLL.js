5128559eb46885676a1f4c629241106e1cbdd352
kelseamc <kelsea.mcall@gmail.com>
2021-03-29 16:55:15 -0400

    data structue notes added

---
+++ DataStructures/DoublyLinkedList/BigODLL.js
@@ -0,0 +1,12 @@
+// Big O of Doubly Linked Lists
+
+
+// Insertion -   O(1)
+
+// Removal -   O(1)
+
+// Searching -   O(N)
+
+// Access -   O(N)
+
+// Technically searching is O(N / 2), but that's still O(N)
---
+++ DataStructures/SinglyLinkedLists/BigOSLL.js
@@ -0,0 +1,10 @@
+// Big O of Singly Linked Lists
+
+
+// Insertion -   O(1)
+
+// Removal -   It depends.... O(1) or O(N)
+
+// Searching -   O(N)
+
+// Access -   O(N)
