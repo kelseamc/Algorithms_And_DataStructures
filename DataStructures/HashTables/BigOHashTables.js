f7bbd7a8e5c64b1927b0a870e57d58af24ddb489
kelseamc <kelsea.mcall@gmail.com>
2021-04-09 12:17:38 -0400

    Hash Tables

---
+++ DataStructures/HashTables/BigOHashTables.js
@@ -0,0 +1,17 @@
+/*************************************** Big O of Hash Tables ***************************************/
+/*
+
+** Best Case ** 
+Insertion => O(1)
+Deletion => O(1)
+Access => O(1)
+
+
+** Worst Case ** 
+- if your hash function doesn't distribute things evenly 
+    Insertion => O(n)
+    Deletion => O(n)
+    Access => O(n)
+
+
+*/
---
+++ DataStructures/HashTables/PsuedocodeHT.js
@@ -0,0 +1,20 @@
+
+/*********** Set ************/
+// accepts a key and value
+// hashes the key 
+// stores the key-value pair in the hash table array via seperate chaining
+
+
+/*********** Get ************/
+// accepts a key 
+// hashes the key
+// retrieves the key-value pair in the hash table 
+// if the key is not found, return undefined
+
+/*********** Keys ************/
+// loop through the hash table array and return an array of all the keys in the table
+
+/*********** Values ************/
+// loop through the hash table array and return an array of all the values in the table
+
+
