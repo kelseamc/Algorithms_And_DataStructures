e313ccf6391600b11341ea6a92fd647643a0e0b9
kelseamc <kelsea.mcall@gmail.com>
2021-04-09 09:52:40 -0400

    binary heap BigO

---
+++ DataStructures/BinaryHeaps/BigOBinaryHeap.js
@@ -0,0 +1,24 @@
+/*************************************** Big O of Binary Heaps ***************************************/
+/* 
+
+
+
+=> Insertion = O(log n)
+=> Removal = O(log n)
+=> Search = O(n)
+
+
+
+
+
+
+
+Every time we double the number of nodes, we are only increasing the time complexity by 1
+
+
+
+** Worst Case **
+
+
+
+*/
